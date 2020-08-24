ab1cc3bc2b642544c8ba20139dd06a0aec69c006
hadapan <69392219+hadapan@users.noreply.github.com>
2020-08-24 11:18:54 +0700

    Create followers.py

---
+++ followers.py
@@ -0,0 +1,70 @@
+#Coded By Zidan-ID\King Mr_Z17
+
+#YouTube Mr_Z17
+
+#Vortex Indonesia
+
+import os,sys,time
+
+ # SILAHKAN COSTUMISASI SENDIRI YA BOSQ #
+
+def main():
+
+    time.sleep(1)
+
+    os.system ('clear')
+
+    print '[96m'
+
+    os.system ('figlet Followers IG')
+
+    print'[92m================================='
+
+    print'[97m Author : [96mJejak Internet '
+
+    print'[97m YouTube   : [96mJejak Internet '
+
+    print'[92m================================='
+
+    print'[92m++++++++++++ [97mM E N U [92m++++++++++++'
+
+    print'[92m[[97m1[92m] [97mFollowers IG'
+
+    print'[92m[[91m0[92m] [91mExit '
+
+    gans = raw_input ('[97m==>[93m ')
+
+    if gans in ['1']:
+
+        time.sleep(1)
+
+        os.system ('git clone https://github.com/BladeKnife/igfollowers ')
+
+        print 
+
+        print (' [+] Silakan ketik cd igfollowers')
+
+        print (' [+] Silakan ketik pip install -r requirement.txt')
+
+        print (' [+] Sialakan ketik python igfol.py')
+
+        exit()
+
+    if gans in ['0']:
+
+        time.sleep(2)
+
+        exit()
+
+    else:
+
+        time.sleep(1)
+
+        print '[97m Pilih Yang Bener [92mBro . . .'
+
+        time.sleep(1)
+
+        main()
+
+main()
+
